0cb2d6e13d27ecca80c935e8b13ffa991ab8c258
Matt Diephouse <matt@diephouse.com>
2011-07-30 13:41:18 -0400

    Add a default stylesheet

--- poplar/views.py
+++ poplar/views.py
@@ -1,8 +1,10 @@
 
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from django.contrib.sites.models import get_current_site
 
 def activity_feed(request):
     site = get_current_site(request)
-    return render_to_response('poplar/activity_feed.html', locals())
+    return render_to_response('poplar/activity_feed.html', locals(),
+                              context_instance=RequestContext(request))
