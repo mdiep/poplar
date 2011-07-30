2fadbd2a879d1ca0a629773494b52bec77238d7f
Matt Diephouse <matt@diephouse.com>
2011-07-30 12:28:19 -0400

    Use the django.contrib.sites framework to determine the name of the site in the default template

--- poplar/views.py
+++ poplar/views.py
@@ -1,5 +1,8 @@
 
 from django.shortcuts import render_to_response
 
+from django.contrib.sites.models import get_current_site
+
 def activity_feed(request):
-    return render_to_response('poplar/activity_feed.html')
+    site = get_current_site(request)
+    return render_to_response('poplar/activity_feed.html', locals())
