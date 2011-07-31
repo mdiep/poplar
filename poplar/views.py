a2c61288a948e1febb490a9fa54f13ae8d5502d4
Matt Diephouse <matt@diephouse.com>
2011-07-31 17:34:08 -0400

    List the people on the Everyone page

--- poplar/views.py
+++ poplar/views.py
@@ -4,12 +4,15 @@ from django.template import RequestContext
 
 from django.contrib.sites.models import get_current_site
 
+from poplar.models import Person
+
 def activity_feed(request):
     site = get_current_site(request)
     return render_to_response('poplar/activity_feed.html', locals(),
                               context_instance=RequestContext(request))
 
 def everyone(request):
-    site = get_current_site(request)
+    site   = get_current_site(request)
+    people = Person.objects.all()
     return render_to_response('poplar/everyone.html', locals(),
                               context_instance=RequestContext(request))
