b532c3476d5187610dc17432a257a6df2526d122
Matt Diephouse <matt@diephouse.com>
2011-07-31 17:31:12 -0400

    Create a Person model

---
+++ poplar/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Person'
+        db.create_table('poplar_person', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+        ))
+        db.send_create_signal('poplar', ['Person'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Person'
+        db.delete_table('poplar_person')
+
+
+    models = {
+        'poplar.person': {
+            'Meta': {'object_name': 'Person'},
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['poplar']
--- poplar/models.py
+++ poplar/models.py
@@ -1,3 +1,18 @@
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name  = models.CharField(max_length=50, blank=True)
+    
+    def __unicode__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+    
+    def clean(self):
+        super(Person,self).clean()
+        if not self.first_name and not self.last_name:
+            raise ValidationError('People must have a first or last name')
+    
+    class Meta:
+        verbose_name_plural = 'people'
