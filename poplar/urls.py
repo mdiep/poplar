9d69fe459288b89470cd85411357bf1dd2c7dc9b
Matt Diephouse <matt@diephouse.com>
2011-07-31 23:35:12 -0400

    Add a person detail view

--- poplar/urls.py
+++ poplar/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('poplar.views',
-    url('^$',         'activity_feed',  name='activity-feed'),
-    url('^people/?$', 'everyone',       name='everyone'),
+    url('^$',               'activity_feed', name='activity-feed'),
+    url('^people/?$',       'everyone',      name='everyone'),
+    url('^people/(\d+)/?$', 'person',        name='person'),
 )
--- poplar/views.py
+++ poplar/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from django.contrib.sites.models import get_current_site
@@ -15,4 +15,10 @@ def everyone(request):
     site   = get_current_site(request)
     people = Person.objects.all()
     return render_to_response('poplar/everyone.html', locals(),
+                              context_instance=RequestContext(request))
+
+def person(request, id):
+    person = get_object_or_404(Person, id=id)
+    site   = get_current_site(request)
+    return render_to_response('poplar/person.html', locals(),
                               context_instance=RequestContext(request))
