acc61ac64bbd61824d8abb2324318b9fb6179e6c
Matt Diephouse <matt@diephouse.com>
2011-08-01 20:44:21 -0400

    Add a page for groups

--- poplar/urls.py
+++ poplar/urls.py
@@ -2,7 +2,8 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('poplar.views',
-    url('^$',               'activity_feed', name='activity-feed'),
-    url('^people/?$',       'everyone',      name='everyone'),
-    url('^people/(\d+)/?$', 'person',        name='person'),
+    url('^$',                  'activity_feed', name='activity-feed'),
+    url('^people/?$',          'everyone',      name='everyone'),
+    url('^people/(\d+)/?$',    'person',        name='person'),
+    url('^groups/([\w-]+)/?$', 'group',         name='group'),
 )
--- poplar/views.py
+++ poplar/views.py
@@ -13,11 +13,7 @@ def activity_feed(request):
                               context_instance=RequestContext(request))
 
 def everyone(request):
-    people = Person.objects.all()
-    groups = Group.objects.all()
-    site   = get_current_site(request)
-    return render_to_response('poplar/everyone.html', locals(),
-                              context_instance=RequestContext(request))
+    return person_list(request, people=Person.objects.all(), title='Everyone')
 
 def person(request, id):
     person = get_object_or_404(Person, id=id)
@@ -25,3 +21,13 @@ def person(request, id):
     site   = get_current_site(request)
     return render_to_response('poplar/person.html', locals(),
                               context_instance=RequestContext(request))
+
+def person_list(request, people, title):
+    groups = Group.objects.all()
+    site   = get_current_site(request)
+    return render_to_response('poplar/person_list.html', locals(),
+                              context_instance=RequestContext(request))
+
+def group(request, slug):
+    group  = get_object_or_404(Group, slug=slug)
+    return person_list(request, people=group.people.all(), title=group.name)
