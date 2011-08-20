95f573c56f4fc896e3c46aad03aa4b9d688570e0
Matt Diephouse <matt@diephouse.com>
2011-08-20 14:17:49 -0400

    Add @login_required to all views

--- poplar/views.py
+++ poplar/views.py
@@ -2,19 +2,23 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
-from django.contrib.sites.models import get_current_site
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.models    import get_current_site
 
 from poplar.models import Person, Group
 
+@login_required
 def activity_feed(request):
     groups = Group.objects.all()
     site   = get_current_site(request)
     return render_to_response('poplar/activity_feed.html', locals(),
                               context_instance=RequestContext(request))
 
+@login_required
 def everyone(request):
     return person_list(request, people=Person.objects.all(), title='Everyone')
 
+@login_required
 def person(request, id):
     person = get_object_or_404(Person, id=id)
     groups = Group.objects.all()
@@ -22,12 +26,14 @@ def person(request, id):
     return render_to_response('poplar/person.html', locals(),
                               context_instance=RequestContext(request))
 
+@login_required
 def person_list(request, people, title):
     groups = Group.objects.all()
     site   = get_current_site(request)
     return render_to_response('poplar/person_list.html', locals(),
                               context_instance=RequestContext(request))
 
+@login_required
 def group(request, slug):
     group  = get_object_or_404(Group, slug=slug)
     return person_list(request, people=group.people.all(), title=group.name)
